9b2b3e0150e3063d9b3ce57dbe1abf026e96e173
timr <timrcode0@gmail.com>
2023-10-06 21:55:15 +0200

    date

--- index.php
+++ index.php
@@ -6,72 +6,143 @@ session_start();
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
+  
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link rel="stylesheet" type="text/css" href="style.css">
     <title>Entrez une ville</title>
 </head>
 <body>
 
-<form method="post">
-    <input type="text" name="ville">
-    <input type="submit" name="submit" value="submit">
-</form>
-
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$token = "b609b585a7ecad3331b8cd0079c3d6c9";
+
     
 if (isset($_POST['submit'])) {
+    if (!empty($_POST['ville'])) {
+        $city = urlencode($_POST['ville']);
+    } else{
+        $city = "paris";
+    } 
 
-    $ville = $_POST['ville'];
+    $token = "b609b585a7ecad3331b8cd0079c3d6c9";
+    $weather = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=$city&lang=fr&appid=$token");
+    $weather_data = json_decode($weather, true);
+    $city = "Paris";
 
-    // Getting geographical info for the city
+    if ($weather_data['cod'] == 200) {
+        $_SESSION['weather'] = [
+            'name' => $weather_data["name"],
+            'temp' => round($weather_data["main"]["temp"] - 273.15),
+            'humidity' => round($weather_data["main"]["humidity"]),
+            'temp_max' => round($weather_data["main"]["temp_max"] - 273.15),
+            'temp_min' => round($weather_data["main"]["temp_min"] - 273.15),
+            'description' => $weather_data['weather'][0]['description'],
+            'timezone' => $weather_data["timezone"]
+            ];
+    } else{
+        $_SESSION['error'] = "error";
+        unset($_SESSION['weather']);
 
-    $meteo = file_get_contents("https://api.openweathermap.org/data/2.5/weather?q=$ville&appid=$token");
-
-    $donnees_meteo = json_decode($meteo, true);
-
-    // Stocker les données nécessaires dans la session ou une autre méthode de stockage
-    
-    $_SESSION['name'] = $donnees_meteo["name"];
-     $donnees_meteo["main"]["temp"] -= 273;
-    $_SESSION['temp'] = round($donnees_meteo["main"]["temp"]) ;
-    $_SESSION['humidity'] = round($donnees_meteo["main"]["humidity"]) ;
-
-
-
-    // Redirigez vers la même page (ou une autre si nécessaire)
+    }
     header('Location: ' . $_SERVER['PHP_SELF']);
     exit;
 }
 
-// Récupérer les données de la session (si disponibles)
-$name = isset($_SESSION['name']) ? $_SESSION['name'] : '';
-$temp = isset($_SESSION['temp']) ? $_SESSION['temp'] : '';
-$humidity = isset($_SESSION['humidity']) ? $_SESSION['humidity'] : '';
+$weather_data = $_SESSION['weather'] ?? [];
+
+$name = $weather_data['name'] ?? "";
+$temp = $weather_data['temp'] ?? "";
+$humidity = $weather_data['humidity'] ?? "";
+$temp_max = $weather_data['temp_max'] ?? "";
+$temp_min = $weather_data['temp_min'] ?? "";
+$description_temp = $weather_data['description'] ?? "";
 
 
-if($name)   {
-$humidity;
-$temp;
-}else{
-    $temp= "";
-    $humidity = "";
+
+
+$error = $_SESSION['error'] ?? "";
+unset($_SESSION['error']);
+
+$imageHTML = '';  
+if($description_temp == "peu nuageux"){
+    $imagePath = "test.png";  
+    $imageHTML = "<img src='$imagePath' alt='' />";
+}
+
+$city_time_str = "";
+
+if($name){
+    $timezone_offset = $weather_data['timezone'] ?? 0;  
+    $hours = intval($timezone_offset / 3600);
+    $minutes = ($timezone_offset % 3600) / 60;
+    $city_time = new DateTime('now', new DateTimeZone("UTC"));
+    $interval = new DateInterval(sprintf("PT%dH%dM", abs($hours), abs($minutes)));
+
+    if ($timezone_offset < 0) {
+        $city_time->sub($interval);
+    } else {
+        $city_time->add($interval);
+    }
+
+    $city_time_str = $city_time->format('d/m/Y - H:i');
+
+} else{
 
 }
 
 
+
 ?>
 
-<div>
-<div>
-        <?php 
-        echo"$humidity <br>";
-        echo $temp; 
-        ?>
-    </div>
 
 
-</div>
+    <header>
+        <img src="images/wallpaper.jpg" alt="wp">
+    </header>
+<main> 
+    <section class="left">
+        <div class="bloc-left">
+            <p> <?php echo htmlspecialchars($temp) ?></p>
+            <div class="name-date">
+            <p> <?php echo htmlspecialchars($name) ?></p>
 
+                <div class="date">
+
+                <p> <?php echo htmlspecialchars($city_time_str) ?></p>
+                </div>
+            </div>
+            <div class="image-temp">
+                <p></p>
+            </div>
+        </div>
+
+    </section>
+
+    <section class="right">
+        <div>
+        <form method="post">
+            <input type="text" name="ville">
+            <input type="submit" name="submit" value="submit">
+        </form>
+              <?php 
+    echo $error;
+                echo $name;
+                echo $temp;
+                echo $humidity;
+                echo $temp_max;
+                echo $temp_min;
+
+            ?>
+
+        </div>
+          
+        
+    </section>
+</main>
+
+    
 </body>
 </html>
